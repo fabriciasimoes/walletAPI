c716efa26b43dd397b74f13e63ef9a7b3abb2af1
fabricia.galibi <fabricia.galibi@prodam.am.gov.br>
2020-02-14 11:06:32 -0400

    configurações iniciais de teste

--- src/test/java/com/wallet/hello.java
+++ src/test/java/com/wallet/hello.java
@@ -1,18 +1,17 @@
 package com.wallet;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ActiveProfiles("test")
-class WalletApplicationTests {
-
+public class hello {
 	@Test
-	public void contextLoads() {
+	public void teste() {
+		assertEquals(1, 1);
 	}
-
 }
